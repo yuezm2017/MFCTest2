f74cf401d905de9820f15287e5cc81caf4a55db4
yuezm2017 <552491782@qq.com>
2019-01-16 10:52:11 +0800

    修改OnOk的处理函数
    修改void CMFCTest2Dlg::OnBnClickedOk()函数的实现。

--- MFCTest2/MFCTest2Dlg.cpp
+++ MFCTest2/MFCTest2Dlg.cpp
@@ -155,6 +155,6 @@ HCURSOR CMFCTest2Dlg::OnQueryDragIcon()
 void CMFCTest2Dlg::OnBnClickedOk()
 {
 	// TODO: Add your control notification handler code here
-	AfxMessageBox(_T("1"));//实现Button Click事件。2019.01.16 YZM
+	AfxMessageBox(_T("222"));//脢碌脧脰Button Click脢脗录镁隆拢2019.01.16 YZM
 	CDialogEx::OnOK();
 }
